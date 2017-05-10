76c0b92c9d294d204b3db45d4a03d0626c295f74
liuqun <liuqun@cb060f17-8521-4c63-8ce0-ac1ab11b5b73>
2017-05-10 09:48:37 +0000

    docs: Add TPM2B_PUBLIC definations and pseudo code examples
    git-svn-id: svn://192.168.0.210/srv/svn/repos/repo-1/trunk@88 cb060f17-8521-4c63-8ce0-ac1ab11b5b73

--- docs/undocumented_types.h
+++ docs/undocumented_types.h
@@ -207,3 +207,108 @@ typedef union {
     } t;
     TPM2B b;
 } TPM2B_PRIVATE;
+
+/**
+ * TPM 2.0 key creation public data storage structure
+ * (with a UINT16 size indicator)
+ *
+ * ```
+ * // Pseudo-code showing the usage of TPM2B_PUBLIC
+ *
+ * TPM2B_PUBLIC keyPublicSettings;
+ *
+ * keyPublicSettings.t.size = 0; // 备忘: 作为Tss2_Sys_Create()函数的输入参数时, 该size字段实际上是无用的,
+ *                               //       无需手动赋值(Tss2_Sys_Create函数内部会自动计算TPM2B_PUBLIC数据块的长度)
+ * keyPublicSettings.t.publicArea.type = TPM_ALG_KEYEDHASH; // 取值可选: TPM_ALG_RSA, TPM_ALG_ECC, TPM_ALG_SYMCIPHER 等等
+ * keyPublicSettings.t.publicArea.nameAlg
+ *         = TPM_ALG_NULL; // TPM 密钥树计算节点名称时使用的哈希算法, 初始值可以不设
+ *                         // 可选取值: TPM_ALG_NULL, TPM_ALG_SHA1, TPM_ALG_SHA256, TPM_ALG_SHA384,
+ *                         //           TPM_ALG_SHA512, TPM_ALG_SM3_256
+ * keyPublicSettings.t.publicArea.objectAttributes.val = (UINT32) 0; // 先清空全部标记位, 然后逐个设置
+ * keyPublicSettings.t.publicArea.objectAttributes.fixedTPM = 1;
+ * keyPublicSettings.t.publicArea.objectAttributes.fixedParent = 1;
+ * keyPublicSettings.t.publicArea.objectAttributes.sensitiveDataOrigin = 1;
+ * keyPublicSettings.t.publicArea.objectAttributes.userWithAuth = 1; // 访问密钥是否须提供用户授权信息
+ * keyPublicSettings.t.publicArea.objectAttributes.restricted = 1; // 是否被限定
+ * keyPublicSettings.t.publicArea.objectAttributes.decrypt = 0;
+ * keyPublicSettings.t.publicArea.objectAttributes.sign = 1; // 是否用于签名
+ * keyPublicSettings.t.publicArea.authPolicy.t.size = 0; // 授权策略(可选)
+ * if (TPM_ALG_KEYEDHASH == keyPublicSettings.t.publicArea.type) {
+ *     // Keyed-Hash 密钥的填法
+ *     keyPublicSettings.t.publicArea.parameters.keyedHashDetail.scheme.scheme
+ *             = TPM_ALG_HMAC; // 可选取值: TPM_ALG_HMAC / TPM_ALG_XOR
+ *     keyPublicSettings.t.publicArea.parameters.keyedHashDetail.scheme.details.hmac.hashAlg
+ *             = TPM_ALG_SHA1; // 可选取值: TPM_ALG_NULL, TPM_ALG_SHA1, TPM_ALG_SHA256, TPM_ALG_SHA384,
+ *                             //           TPM_ALG_SHA512, TPM_ALG_SM3_256
+ * } else if (TPM_ALG_RSA == keyPublicSettings.t.publicArea.type) {
+ *     // RSA 密钥的填法
+ *     keyPublicSettings.t.publicArea.parameters.rsaDetail.symmetric.algorithm = TPM_ALG_AES; // 内置对称密钥类型
+ *     keyPublicSettings.t.publicArea.parameters.rsaDetail.symmetric.keyBits.aes = 128; // 对称密钥长度
+ *     keyPublicSettings.t.publicArea.parameters.rsaDetail.symmetric.mode.aes = TPM_ALG_ECB;
+ *     keyPublicSettings.t.publicArea.parameters.rsaDetail.scheme.scheme
+ *             = TPM_ALG_NULL; // 取值可选: TPM_ALG_NULL, TPM_ALG_RSAPSS, TPM_ALG_RSASSA, TPM_ALG_RSAES, TPM_ALG_OAEP
+ *     keyPublicSettings.t.publicArea.parameters.rsaDetail.keyBits = 2048; // RSA 密钥长度: 1024 / 2048 / 4096
+ *     keyPublicSettings.t.publicArea.parameters.rsaDetail.exponent = 0;
+ *     keyPublicSettings.t.publicArea.unique.rsa.t.size = 0;
+ * }
+ * keyPublicSettings.t.publicArea.unique.keyedHash.t.size = 0;
+ * keyPublicSettings.t.publicArea.unique.keyedHash.t.buffer[0] = '\0'; // 填零便于测试
+ *
+ * TPM2B_PUBLIC keyPublicResult;
+ *
+ * // Call Tss2_Sys_Create()
+ * Tss2_Sys_Create(
+ *     sysContext,
+ *     parentHandle,
+ *     &cmdAuthsArray,
+ *     &inSensitive,
+ *     &keyPublicSettings, // Here TPM2B_PUBLIC is used as an input templete
+ *     &outsideInfo,
+ *     &creationPCR,
+ *     &outPrivate,
+ *     &keyPublicResult, // Here TPM2B_PUBLIC is used as an output result of the input templete
+ *     &creationData,
+ *     &creationHash,
+ *     &creationTicket,
+ *     &rspAuthsArray
+ *     );
+ *
+ * TPM2B_PUBLIC& inPublic = keyPublic; // C++ coding syntax
+ *
+ * // Call Tss2_Sys_Load()
+ * Tss2_Sys_Load(
+ *     sysContext,
+ *     parentHandle,
+ *     &cmdAuthsArray,
+ *     &inPrivate,
+ *     &inPublic, // Here TPM2B_PUBLIC is used as an input parameter for Tss2_Sys_Load()
+ *     &keyHandle,
+ *     &keyName,
+ *     &rspAuthsArray
+ *     );
+ * ```
+ *
+ * @details
+ * The detail data inside TPM2B_PUBLIC was defined in the following structures:
+ * @see TPMT_PUBLIC
+ * @see TPMA_OBJECT
+ * @see TPMU_PUBLIC_PARMS
+ * @see TPMS_KEYEDHASH_PARMS / TPMS_SYMCIPHER_PARMS / TPMS_RSA_PARMS / TPMS_ECC_PARMS / TPMS_ASYM_PARMS
+ * @see TPMU_PUBLIC_ID
+ *
+ * @see https://trustedcomputinggroup.org/wp-content/uploads/TPM-Rev-2.0-Part-2-Structures-01.38.pdf ,
+ * Chapter 12.2.5
+ * Table 191: Definition of TPM2B_PUBLIC Structure
+ */
+typedef union {
+    struct {
+        /// @brief A reserved UINT16 size field
+        /// @note The value of this field will always be ignored by every Tss2_Sys_XXXX methord, including Tss2_Sys_Create() and Tss2_Sys_Load()
+        UINT16 size;
+
+        /// @brief The key's public info
+        /// (including its cypher algorithm ID, key length, public key info and key name hash methord)
+        TPMT_PUBLIC publicArea;
+    } t;
+    TPM2B b;
+} TPM2B_PUBLIC;
