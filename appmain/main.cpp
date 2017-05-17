7c09988bad5b8d3c3fd2cb1ddbe292f7ada4a97d
liuqun <liuqun@cb060f17-8521-4c63-8ce0-ac1ab11b5b73>
2017-05-17 12:46:38 +0000

    清理代码
    1. 删除 main.cpp 模板中的 DoMyTestsWithTctiContext() 和 DoMyTestsWithSysContext()
    2. 清理 main() 函数
    3. 删除多个 DebugPrintf() 函数
    4. 删除 InitSocketTctiContext() TeardownTctiContext() GetTctiContextSize()
    
    git-svn-id: svn://192.168.0.210/srv/svn/repos/repo-1/trunk@102 cb060f17-8521-4c63-8ce0-ac1ab11b5b73

--- appmain/main.cpp
+++ appmain/main.cpp
@@ -8,26 +8,6 @@ using namespace std;
 #include <sapi/tpm20.h>
 #include <tcti/tcti_socket.h>
 
-/* 自定义函数 */
-static void DoMyTestsWithTctiContext(TSS2_TCTI_CONTEXT *pTctiContext);
-static void DoMyTestsWithSysContext(TSS2_SYS_CONTEXT *pSysContext);
-
-extern "C"
-{
-
-int DebugPrintf(printf_type type, const char *format, ...);
-int DebugPrintfCallback(void *data, printf_type type, const char *format, ...);
-
-void DebugPrintBuffer(printf_type type, UINT8 *command_buffer, UINT32 cnt1);
-int DebugPrintBufferCallback(void *data, printf_type type, UINT8 *buffer,
-        UINT32 length);
-
-TSS2_RC InitSocketTctiContext(const TCTI_SOCKET_CONF *conf,
-        TSS2_TCTI_CONTEXT **ppTctiContext);
-void TeardownTctiContext(TSS2_TCTI_CONTEXT **ppTctiContext);
-
-} /* End of extern "C" */
-
 /* 排版格式: 以下函数均使用4个空格缩进，不使用Tab缩进 */
 
 static void PrintHelp()
@@ -40,16 +20,9 @@ static void PrintHelp()
 
 int main(int argc, char *argv[])
 {
-    TSS2_RC rval;
-    TCTI_SOCKET_CONF rmInterfaceConfig;
-    TSS2_TCTI_CONTEXT *pTctiContext;
     int count;
-
-    rmInterfaceConfig.hostname = DEFAULT_HOSTNAME;
-    rmInterfaceConfig.port = DEFAULT_RESMGR_TPM_PORT;
-    rmInterfaceConfig.logCallback = DebugPrintfCallback;
-    rmInterfaceConfig.logBufferCallback = DebugPrintBufferCallback;
-    rmInterfaceConfig.logData = NULL;
+    char *hostname = DEFAULT_HOSTNAME;
+    uint16_t port = DEFAULT_RESMGR_TPM_PORT;
 
     count = 1;
     while (count < argc)
@@ -61,7 +34,7 @@ int main(int argc, char *argv[])
                 PrintHelp();
                 return 1;
             }
-            rmInterfaceConfig.hostname = argv[count + 1];  // 暂时不检查无效的输入参数
+            hostname = argv[count + 1];  // 暂时不检查无效的输入参数
             count += 2;
         }
         else if (0 == strcmp(argv[count], "-rmport"))
@@ -71,7 +44,7 @@ int main(int argc, char *argv[])
                 PrintHelp();
                 return 1;
             }
-            rmInterfaceConfig.port = strtoul(argv[count + 1], NULL, 10); // 暂时不检查无效的输入参数
+            port = strtoul(argv[count + 1], NULL, 10); // 暂时不检查无效的输入参数
             count += 2;
         }
         else
@@ -84,190 +57,5 @@ int main(int argc, char *argv[])
     // 其中包括 [-rmhost IP地址] 和 [-rmport 端口号]
     // 如果不指定命令行参数, 则会直接连接到本机 IP 地址默认端口上运行的资源管理器
 
-    /**/
-    rval = InitSocketTctiContext(&rmInterfaceConfig, &pTctiContext);
-    if (rval != TSS2_RC_SUCCESS)
-    {
-        // Note:
-        // 当前 InitSocketTcti() 返回的 TSS2_RC 值并未严格按照 TPM2.0 规范指示错误原因。
-        // 错误返回值实测结果: 无法连接服务器端 IP 地址或默认端口号无法建立套接字时, 返回值均等于 1
-        DebugPrintf(NO_PREFIX,
-                "TCTI context initialization failed with error return code=0x%x\n",
-                rval);
-        return (-1);
-    }
-    else if (!pTctiContext)
-    {
-        DebugPrintf(NO_PREFIX, "TCTI context initialization failed\n");
-        return (-1);
-    }
-
-    /* 使用前面创建的 TCTI 上下文对象进一步创建其他测试内容 */
-    DoMyTestsWithTctiContext(pTctiContext);
-
-    /* 测试结束后销毁 TCTI 上下文对象 */
-    TeardownTctiContext(&pTctiContext);
     return (0);
 }
-
-static void DoMyTestsWithTctiContext(TSS2_TCTI_CONTEXT *pTctiContext)
-{
-    TSS2_RC rval;
-    TSS2_ABI_VERSION abiVersion;
-    TSS2_SYS_CONTEXT *pSysContext;
-    size_t contextSize;
-
-    contextSize = Tss2_Sys_GetContextSize(0);
-    pSysContext = (TSS2_SYS_CONTEXT *) malloc(contextSize);
-    if (!pSysContext)
-    {
-        DebugPrintf(NO_PREFIX,
-                "Unable to allocate enough memory: malloc() failed.\n");
-        DebugPrintf(NO_PREFIX, "Exiting...\n");
-        return;
-    }
-
-    abiVersion.tssCreator = TSSWG_INTEROP;
-    abiVersion.tssFamily = TSS_SAPI_FIRST_FAMILY;
-    abiVersion.tssLevel = TSS_SAPI_FIRST_LEVEL;
-    abiVersion.tssVersion = TSS_SAPI_FIRST_VERSION;
-
-    rval = Tss2_Sys_Initialize(pSysContext, contextSize, pTctiContext,
-            &abiVersion);
-
-    if (rval != TSS2_RC_SUCCESS)
-    {
-        free(pSysContext);
-        DebugPrintf(NO_PREFIX,
-                "Unable to initialize system level API context:");
-        DebugPrintf(NO_PREFIX,
-                "Tss2_Sys_Initialize() returns error code 0x%06X.\n", rval);
-        DebugPrintf(NO_PREFIX, "Exiting...\n");
-        return;
-    }
-
-    DoMyTestsWithSysContext(pSysContext);
-
-    /* Clean up the context when all tests ends */
-    Tss2_Sys_Finalize(pSysContext);
-    free(pSysContext);
-    pSysContext = NULL;
-    return;
-}
-
-static void DoMyTestsWithSysContext(TSS2_SYS_CONTEXT *pSysContext)
-{
-}
-
-/* 调试专用函数 */
-#include <stdarg.h>
-
-extern "C"
-{
-
-int DebugPrintf(printf_type type, const char *format, ...)
-{
-    va_list args;
-    int rval = 0;
-
-    if (type == RM_PREFIX)
-    {
-        printf("||  ");
-    }
-    va_start(args, format);
-    rval = vprintf(format, args);
-    va_end(args);
-
-    return rval;
-}
-
-int DebugPrintfCallback(void *data, printf_type type, const char *format, ...)
-{
-    va_list args;
-    int rval = 0;
-
-    if (type == RM_PREFIX)
-    {
-        DebugPrintfCallback(data, NO_PREFIX, "||  ");
-    }
-    va_start(args, format);
-    rval = vprintf(format, args);
-    va_end(args);
-
-    return rval;
-}
-
-void DebugPrintBuffer(printf_type type, UINT8 *buffer, UINT32 length)
-{
-    UINT32 i;
-
-    for (i = 0; i < length; i++)
-    {
-        if ((i % 16) == 0)
-        {
-            DebugPrintf(NO_PREFIX, "\n");
-            if (type == RM_PREFIX)
-            {
-                DebugPrintf(NO_PREFIX, "||  ");
-            }
-        }
-
-        DebugPrintf(NO_PREFIX, "%2.2x ", buffer[i]);
-    }
-    DebugPrintf(NO_PREFIX, "\n\n");
-    fflush (stdout);
-}
-
-int DebugPrintBufferCallback(void *data, printf_type type, UINT8 *buffer,
-        UINT32 length)
-{
-    DebugPrintBuffer(type, buffer, length);
-    return 0;
-}
-
-} /* End of extern "C" */
-/* End of 调试专用函数 */
-
-/* 自定义: 对TCTI底层接口进行的封装 */
-extern "C"
-{
-
-static size_t GetSocketTctiContextSize()
-{
-    TCTI_SOCKET_CONF emptyConf;
-    size_t size;
-    TSS2_RC err;
-
-    err = InitSocketTcti(NULL, &size, &emptyConf, 0);
-    if (err)
-    {
-        fprintf(stderr,
-                "Error: Failed to fetch size of TSS2_TCTI_CONTEXT from libtcti-socket\n");
-        fprintf(stderr, "(This error should NEVER happen)\n");
-        exit(0);
-    }
-    return size;
-}
-
-TSS2_RC InitSocketTctiContext(const TCTI_SOCKET_CONF *conf,
-        TSS2_TCTI_CONTEXT **tcti_context)
-{
-    size_t size;
-
-    size = GetSocketTctiContextSize();
-    *tcti_context = (TSS2_TCTI_CONTEXT *) malloc(size);
-    return InitSocketTcti(*tcti_context, &size, conf, 0);
-}
-
-void TeardownTctiContext(TSS2_TCTI_CONTEXT **tctiContext)
-{
-    if (*tctiContext != NULL)
-    {
-        tss2_tcti_finalize(*tctiContext);
-        free(*tctiContext);
-        *tctiContext = NULL;
-    }
-}
-
-} /* End of extern "C" */
-/* End of 自定义: 对TCTI底层接口进行的封装 */
