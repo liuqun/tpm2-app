2ccdc8100bf3e49042d947d7f633356e761758cd
liuqun <liuqun@cb060f17-8521-4c63-8ce0-ac1ab11b5b73>
2017-05-17 12:46:44 +0000

    创建 AppFramework 测试框架
    git-svn-id: svn://192.168.0.210/srv/svn/repos/repo-1/trunk@103 cb060f17-8521-4c63-8ce0-ac1ab11b5b73

--- appmain/main.cpp
+++ appmain/main.cpp
@@ -18,8 +18,70 @@ static void PrintHelp()
     printf("-rmport 手动指定运行资源管理器的主机端口号 (默认值: %d)\n", DEFAULT_RESMGR_TPM_PORT);
 }
 
+/// @class TPMCommand
+/// @details 所有 TPM 命令对象的抽象基类
+class TPMCommand
+{
+public:
+    TPMCommand()
+    {
+    }
+    virtual ~TPMCommand()
+    {
+    }
+};
+
+/// @namespace TPMCommands
+/// @details 盛放各种 TPM 命令对象
+namespace TPMCommands
+{
+
+class Startup: public TPMCommand
+{
+};
+
+class Shutdown: public TPMCommand
+{
+};
+
+}
+
+
+/// @class MyAppFreamework
+/// @details
+class MyAppFramework
+{
+    size_t m_TCTIContextSize;
+    size_t m_SystemAPIContextSize;
+    TSS2_TCTI_CONTEXT *m_tctiContext; // 推荐使用成员变量 m_tctiContext (避免在主程序中使用全局变量 tctiContext).
+    TSS2_SYS_CONTEXT *m_sysContext; // 推荐使用成员变量 m_sysContext (避免在主程序中使用全局变量 sysContext).
+
+public:
+    /** 构造函数 */
+    MyAppFramework();
+    /** 析构函数 */
+    ~MyAppFramework();
+    /** 与本地默认端口上运行的 TSS resource manager 守护进程建立连接 */
+    void connectToDefaultLocalResourceManager();
+    /** 与任意远程或本地 TSS resource manager 守护进程建立连接 */
+    void connectToResourceManager(const char *hostname=DEFAULT_HOSTNAME, uint16_t port=DEFAULT_RESMGR_TPM_PORT);
+    /** 发送命令帧 */
+    void sendCommand(TPMCommand *cmd);
+    /**
+     * 取回应答帧
+     *
+     * @note 若该函数执行成功, 返回的数据将被写入之前调用 sendCommand() 时指定的 cmd 对象
+     */
+    void fetchResponse(int32_t timeout=-1 ///< 超时选项. 默认使用负数表示阻塞等待, 直到服务器端相应或者发生严重错误
+            );
+    /** 切断与 TPM resource manager 之间的连接 */
+    void disconnectResourceManager();
+};
+
+#include <exception>
 int main(int argc, char *argv[])
 {
+    MyAppFramework framework;
     int count;
     char *hostname = DEFAULT_HOSTNAME;
     uint16_t port = DEFAULT_RESMGR_TPM_PORT;
@@ -52,10 +114,183 @@ int main(int argc, char *argv[])
             PrintHelp();
             return -1;
         }
+        // 以上代码提供了一组简单的命令行参数便于调试:
+        // 其中包括 [-rmhost IP地址] 和 [-rmport 端口号]
+        // 如果不指定命令行参数, 则会直接连接到本机 IP 地址默认端口上运行的资源管理器
     }
-    // 以上代码提供了一组简单的命令行参数便于调试:
-    // 其中包括 [-rmhost IP地址] 和 [-rmport 端口号]
-    // 如果不指定命令行参数, 则会直接连接到本机 IP 地址默认端口上运行的资源管理器
 
+    // 测试开始, 首先建立与 TSS resource manager 连接
+    framework.connectToResourceManager(hostname, port);
+    // ----------------------------------
+    printf("%s\n", "测试 Startup 命令");
+    TPMCommand *startup = new TPMCommands::Startup();
+    try
+    {
+        framework.sendCommand(startup);
+        framework.fetchResponse();
+    }
+    catch(std::exception& e)
+    {
+        fprintf(stderr, "Error: %s\n", e.what());
+    }
+    delete startup;
+    // ----------------------------------
+    printf("%s\n", "测试 Shutdown 命令");
+    TPMCommand *shutdown = new TPMCommands::Shutdown();
+    try
+    {
+        framework.sendCommand(shutdown);
+        framework.fetchResponse();
+    }
+    catch (std::exception& e)
+    {
+        fprintf(stderr, "Error: %s\n", e.what());
+    }
+    delete shutdown;
+    // 测试结束需要手动切断与 TSS resource manager 之间的连接
+    framework.disconnectResourceManager();
     return (0);
+}
+
+#include <cassert>
+
+static size_t GetSocketTctiContextSize();
+
+MyAppFramework::MyAppFramework()
+{
+    m_TCTIContextSize = GetSocketTctiContextSize();
+    m_tctiContext = (TSS2_TCTI_CONTEXT *) malloc(m_TCTIContextSize);
+    assert(m_tctiContext);
+    if (!m_tctiContext)
+    {
+        exit (EXIT_FAILURE);
+    }
+    memset(m_tctiContext, 0x00, m_TCTIContextSize);
+
+    m_SystemAPIContextSize = Tss2_Sys_GetContextSize(0);
+    m_sysContext = (TSS2_SYS_CONTEXT *) malloc(m_SystemAPIContextSize);
+    if (!m_sysContext)
+    {
+        exit (EXIT_FAILURE);
+    }
+    memset(m_sysContext, 0x00, m_SystemAPIContextSize);
+}
+
+MyAppFramework::~MyAppFramework()
+{
+    /* 销毁 TCTI 上下文对象 */
+    assert(m_tctiContext);
+    free(m_tctiContext);
+
+    /* 销毁 System API 上下文对象 */
+    assert(m_sysContext);
+    free(m_sysContext);
+}
+
+void MyAppFramework::connectToResourceManager(const char *hostname, uint16_t port)
+{
+    TCTI_SOCKET_CONF conf;
+    conf.hostname = (char *) hostname;
+    conf.port = port;
+    conf.logCallback = NULL;
+    conf.logBufferCallback = NULL;
+    conf.logData = NULL;
+
+    TSS2_RC err;
+    err = InitSocketTcti(m_tctiContext, &m_TCTIContextSize, &conf, 0);
+    if (err)
+    {
+        // TODO: throw/raise an expection to the up level
+    }
+
+    TSS2_ABI_VERSION abiVersion;
+    abiVersion.tssCreator = TSSWG_INTEROP;
+    abiVersion.tssFamily = TSS_SAPI_FIRST_FAMILY;
+    abiVersion.tssLevel = TSS_SAPI_FIRST_LEVEL;
+    abiVersion.tssVersion = TSS_SAPI_FIRST_VERSION;
+
+    TSS2_RC err2;
+    err = Tss2_Sys_Initialize(
+            m_sysContext,
+            m_SystemAPIContextSize,
+            m_tctiContext,
+            &abiVersion);
+    if (err2)
+    {
+        // TODO: throw/raise an expection to the up level
+    }
+}
+
+void MyAppFramework::connectToDefaultLocalResourceManager()
+{
+    connectToResourceManager();
+}
+
+void MyAppFramework::sendCommand(TPMCommand *cmd)
+{
+    // TODO: 解读 cmd 对象
+    // cmd.handle
+    // cmd.authSession
+    // cmd.authValue
+    // cmd.pushAuthSessionDecryptMethodOfCommandPacket 向 TPM 模块指明如何解码命令帧加密字段
+    // cmd.pushAuthSessionEncryptMethodOfResponsePacket 向 TPM 模块指明如何回传应答桢加密字段
+    // cmd.inputParameterList 输入参数列表
+    // cmd.outputParameterList 输出参数列表
+    // cmd.prepareFunc 作为函数指针, 指向相应的 TSS 软件栈 Tss2_Sys_XXXX_Prepare() 函数
+
+    // 异步发送命令帧
+    TSS2_RC err = Tss2_Sys_ExecuteAsync(m_sysContext);
+    if (err)
+    {
+        // TODO: throw/raise an expection to the up level
+    }
+}
+
+void MyAppFramework::fetchResponse(int32_t timeout)
+{
+    if (timeout < 0)
+    {
+        timeout = TSS2_TCTI_TIMEOUT_BLOCK;
+    }
+    TSS2_RC err = Tss2_Sys_ExecuteFinish(m_sysContext, timeout);
+    if (err)
+    {
+        // TODO: throw/raise an expection to the up level
+    }
+}
+
+void MyAppFramework::disconnectResourceManager()
+{
+    assert(m_sysContext);
+    Tss2_Sys_Finalize(m_sysContext);
+
+    assert(m_tctiContext);
+    const TSS2_TCTI_CONTEXT_COMMON_CURRENT *ctx =
+            (TSS2_TCTI_CONTEXT_COMMON_CURRENT *) m_tctiContext;
+    if (!(ctx->finalize))
+    {
+        return;
+    }
+    else if (ctx->version < 1)
+    {
+        return;
+    }
+    ctx->finalize(m_tctiContext);
+}
+
+size_t GetSocketTctiContextSize()
+{
+    TCTI_SOCKET_CONF emptyConf;
+    size_t size;
+    TSS2_RC err;
+
+    err = InitSocketTcti(NULL, &size, &emptyConf, 0);
+    if (err)
+    {
+        fprintf(stderr,
+                "Error: Failed to fetch size of TSS2_TCTI_CONTEXT from libtcti-socket\n");
+        fprintf(stderr, "(This error should NEVER happen)\n");
+        exit(0);
+    }
+    return size;
 }
