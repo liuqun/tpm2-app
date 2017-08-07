e4610e055ce6adb0993bc619b243cb87fa617725
liuqun <liuqun@cb060f17-8521-4c63-8ce0-ac1ab11b5b73>
2017-08-07 09:44:32 +0000

    appmain: 创建客户端类class Client
    头文件Client.h
    cpp文件 Client.cpp
    
    git-svn-id: svn://192.168.0.210/srv/svn/repos/repo-1/trunk@226 cb060f17-8521-4c63-8ce0-ac1ab11b5b73

---
+++ appmain/Client.cpp
@@ -0,0 +1,266 @@
+/* encoding: utf-8 */
+// Copyright (c) 2017, 青岛中怡智能安全研究院有限公司
+// All rights reserved.
+#include <cstdio>
+#include <cstdlib> // malloc()/free()
+#include <cassert> // assert()
+#include <stdexcept>
+using std::exception;
+#include <sapi/tpm20.h>
+#include <tcti/tcti_socket.h>
+#include <tcti/tcti_device.h>
+#include "TPMCommand.h"
+#include "Client.h"
+
+/* 排版格式: 以下函数均使用4个空格缩进，不使用Tab缩进 */
+
+static size_t GetSocketTctiContextSize() {
+    TCTI_SOCKET_CONF emptyConf;
+    size_t size;
+    TSS2_RC err;
+
+    err = InitSocketTcti(NULL, &size, &emptyConf, 0);
+    if (err) {
+        fprintf(stderr,
+                "Error: Failed to fetch size of TSS2_TCTI_CONTEXT from libtcti-socket\n");
+        fprintf(stderr, "(This error should NEVER happen)\n");
+        return 0;
+    }
+    return size;
+}
+
+static size_t GetDeviceTctiContextSize() {
+    TCTI_DEVICE_CONF emptyConf;
+    size_t size;
+    TSS2_RC err;
+
+    err = InitDeviceTcti(NULL, &size, &emptyConf);
+    if (err)
+    {
+        fprintf(stderr,
+                "Error: Failed to fetch size of TSS2_TCTI_CONTEXT from libtcti-device\n");
+        fprintf(stderr, "(This error should NEVER happen)\n");
+        return 0;
+    }
+    return size;
+}
+
+static size_t GetMax(size_t a, size_t b) {
+    size_t max;
+
+    max = (a >= b)? a:b;
+    return (max);
+}
+
+size_t GetMaxTctiContextSize() {
+    size_t max;
+
+    max = GetMax(GetDeviceTctiContextSize(), GetSocketTctiContextSize());
+    return (max);
+}
+
+Client::Client() {
+    m_tctiContextSize = GetMaxTctiContextSize();
+    m_tctiContext = (TSS2_TCTI_CONTEXT *) malloc(m_tctiContextSize);
+    assert(m_tctiContext);
+    if (!m_tctiContext) {
+        fprintf(stderr, "Error: malloc() failed to allocate dynamic memory\n");
+        exit (EXIT_FAILURE);
+    }
+    memset(m_tctiContext, 0x00, m_tctiContextSize);
+
+    m_sysContextSize = Tss2_Sys_GetContextSize(0);
+    m_sysContext = (TSS2_SYS_CONTEXT *) malloc(m_sysContextSize);
+    if (!m_sysContext)
+    {
+        fprintf(stderr, "Error: malloc() failed to allocate dynamic memory\n");
+        exit (EXIT_FAILURE);
+    }
+    memset(m_sysContext, 0x00, m_sysContextSize);
+    m_pLastCommand = NULL;
+    m_contextInitializer = NULL;
+}
+
+Client::~Client() {
+    /* 销毁 TCTI 上下文对象 */
+    assert(m_tctiContext);
+    free(m_tctiContext);
+
+    /* 销毁 System API 上下文对象 */
+    assert(m_sysContext);
+    free(m_sysContext);
+}
+
+void Client::sendCommandAndWaitUntilResponseIsFetched(TPMCommand& cmd) {
+    int timeout = TSS2_TCTI_TIMEOUT_BLOCK;
+    try {
+        sendCommand(cmd);
+        m_pLastCommand = &cmd;
+        fetchResponse(timeout);
+    } catch (std::exception e) {
+        // TODO: 处理发送命令桢或接收响应桢中可能遇到的异常情况, 例如响应超时等
+    }
+}
+
+void Client::sendCommand(TPMCommand& cmd) {
+    cmd.buildCmdPacket(m_sysContext); // 调用相应的 TSS 软件栈 Tss2_Sys_XXXX_Prepare() 函数
+
+    // 异步发送命令帧
+    TSS2_RC err = Tss2_Sys_ExecuteAsync(m_sysContext);
+    if (err) {
+        fprintf(stderr, "Error: Cannot send command packet: Tss2_Sys_ExecuteAsync() returns 0x%X\n", (int) err);
+        // TODO: throw/raise an expection to the up level
+    }
+    m_pLastCommand = &cmd;
+}
+
+void Client::fetchResponse(int32_t timeout) {
+    if (!m_pLastCommand) {
+        fprintf(stderr, "Error: No response packet to fetch\n");
+        // Note: throw/raise an expection to the up level
+        const TSS2_RC AppError=(TSS2_APP_ERROR_LEVEL|TSS2_BASE_RC_BAD_SEQUENCE);
+        throw AppError;
+    }
+
+    if (timeout < 0) {
+        timeout = TSS2_TCTI_TIMEOUT_BLOCK;
+    }
+    TSS2_RC err = Tss2_Sys_ExecuteFinish(m_sysContext, timeout);
+    if (err) {
+        fprintf(stderr, "Error: Cannot fetch response packet: Tss2_Sys_ExecuteFinish() returns err = 0x%X\n", err);
+        // TODO: throw/raise an expection to the up level
+        throw err;
+    }
+    m_pLastCommand->unpackRspPacket(m_sysContext); // 调用相应的 TSS 软件栈 Tss2_Sys_XXXX_Complete() 函数
+}
+
+void Client::disconnect() {
+    assert(m_sysContext);
+    Tss2_Sys_Finalize(m_sysContext);
+
+    assert(m_tctiContext);
+    const TSS2_TCTI_CONTEXT_COMMON_CURRENT *ctx =
+            (TSS2_TCTI_CONTEXT_COMMON_CURRENT *) m_tctiContext;
+    if (!(ctx->finalize)) {
+        return;
+    }
+    else if (ctx->version < 1) {
+        return;
+    }
+    ctx->finalize(m_tctiContext);
+}
+
+void Client::connect() {
+    assert(m_contextInitializer);
+    m_contextInitializer->initializerCallbackFunc(m_tctiContext, m_tctiContextSize, m_sysContext, m_sysContextSize);
+}
+
+void Client::setContextInitializer(ContextInitializer& initializer) {
+    m_contextInitializer = &initializer;
+}
+
+// 以下为 TCTI / System API 上下文初始化工具
+
+// 回调函数原型 initializerCallbackFunc()
+void ContextInitializer::initializerCallbackFunc(TSS2_TCTI_CONTEXT *tctiContext, size_t tctiContextSize, TSS2_SYS_CONTEXT *sysContext, size_t sysContextSize)
+{
+}
+
+// 构造函数 SocketBasedContextInitializer()
+SocketBasedContextInitializer::SocketBasedContextInitializer(const char *hostname, uint16_t port)
+{
+    if (!hostname)
+    {
+        hostname = "127.0.0.1";
+    }
+    m_hostname = hostname;
+    m_port = port;
+}
+
+// 析构函数 ~SocketBasedContextInitializer()
+SocketBasedContextInitializer::~SocketBasedContextInitializer()
+{
+}
+
+// 回调函数 initializerCallbackFunc()
+void SocketBasedContextInitializer::initializerCallbackFunc(TSS2_TCTI_CONTEXT *tctiContext, size_t tctiContextSize, TSS2_SYS_CONTEXT *sysContext, size_t sysContextSize)
+{
+    TCTI_SOCKET_CONF conf;
+    conf.hostname = (const char *) m_hostname;
+    conf.port = m_port;
+    conf.logCallback = NULL;
+    conf.logBufferCallback = NULL;
+    conf.logData = NULL;
+
+    TSS2_RC tctiError;
+    tctiError = InitSocketTcti(tctiContext, &tctiContextSize, &conf, 0);
+    if (tctiError) {
+        fprintf(stderr, "Error: InitSocketTcti() returns 0x%X\n", (int) tctiError);
+        // TODO: throw/raise an expection to the up level
+    }
+
+    TSS2_ABI_VERSION abiVersion;
+    abiVersion.tssCreator = TSSWG_INTEROP;
+    abiVersion.tssFamily = TSS_SAPI_FIRST_FAMILY;
+    abiVersion.tssLevel = TSS_SAPI_FIRST_LEVEL;
+    abiVersion.tssVersion = TSS_SAPI_FIRST_VERSION;
+
+    TSS2_RC sapiError;
+    sapiError = Tss2_Sys_Initialize(
+            sysContext,
+            sysContextSize,
+            tctiContext,
+            &abiVersion);
+    if (sapiError) {
+        fprintf(stderr, "Error: Tss2_Sys_Initialize() returns 0x%X\n", (int) sapiError);
+        // TODO: throw/raise an expection to the up level
+    }
+}
+
+// 构造函数 DeviceBasedContextInitializer()
+DeviceBasedContextInitializer::DeviceBasedContextInitializer(const char *device)
+{
+    if (!device)
+    {
+        device = "/dev/tpm0";
+    }
+    m_device = device;
+}
+
+// 析构函数 ~DeviceBasedContextInitializer()
+DeviceBasedContextInitializer::~DeviceBasedContextInitializer()
+{
+}
+
+// 回调函数 initializerCallbackFunc()
+void DeviceBasedContextInitializer::initializerCallbackFunc(TSS2_TCTI_CONTEXT *tctiContext, size_t tctiContextSize, TSS2_SYS_CONTEXT *sysContext, size_t sysContextSize)
+{
+    TCTI_DEVICE_CONF conf;
+    conf.device_path = (const char *) m_device;
+    conf.logCallback = NULL;
+    conf.logData = NULL;
+
+    TSS2_RC tctiError;
+    tctiError = InitDeviceTcti(tctiContext, &tctiContextSize, &conf);
+    if (tctiError) {
+        fprintf(stderr, "Error: InitSocketTcti() returns 0x%X\n", (int) tctiError);
+        // TODO: throw/raise an expection to the up level
+    }
+
+    TSS2_ABI_VERSION abiVersion;
+    abiVersion.tssCreator = TSSWG_INTEROP;
+    abiVersion.tssFamily = TSS_SAPI_FIRST_FAMILY;
+    abiVersion.tssLevel = TSS_SAPI_FIRST_LEVEL;
+    abiVersion.tssVersion = TSS_SAPI_FIRST_VERSION;
+
+    TSS2_RC sapiError;
+    sapiError = Tss2_Sys_Initialize(
+            sysContext,
+            sysContextSize,
+            tctiContext,
+            &abiVersion);
+    if (sapiError) {
+        fprintf(stderr, "Error: Tss2_Sys_Initialize() returns 0x%X\n", (int) sapiError);
+        // TODO: throw/raise an expection to the up level
+    }
+}
---
+++ appmain/Client.h
@@ -0,0 +1,107 @@
+/* encoding: utf-8 */
+// Copyright (c) 2017, 青岛中怡智能安全研究院有限公司
+// All rights reserved.
+
+#ifndef CLIENT_H_
+#define CLIENT_H_
+
+#ifndef __cplusplus
+#warning // Only C++ is supported. Please DON'T include this file from *.c!
+#endif
+
+#include <sapi/tpm20.h>
+#include "TPMCommand.h"
+
+#ifdef __cplusplus
+
+/// 上下文初始化工具抽象基类: 仅声明一个回调函数接口原型 initializerCallbackFunc(), 由她的子类去集成实现该接口.
+class ContextInitializer {
+public:
+    /** 回调接口原型 */
+    virtual void initializerCallbackFunc(TSS2_TCTI_CONTEXT *tctiContext, size_t tctiContextSize, TSS2_SYS_CONTEXT *sysContext, size_t sysContextSize);
+};
+
+/// TPM客户端
+class Client
+{
+private:
+    ContextInitializer *m_contextInitializer; ///<
+    TSS2_TCTI_CONTEXT *m_tctiContext; ///< 成员变量 m_tctiContext (取代全局变量 tctiContext, 降低耦合度).
+    TSS2_SYS_CONTEXT *m_sysContext; ///< 成员变量 m_sysContext (取代全局变量 sysContext, 降低耦合度).
+    size_t m_tctiContextSize;
+    size_t m_sysContextSize;
+
+public:
+    /** 构造函数 */
+    Client();
+    /** 析构函数 */
+    virtual ~Client();
+    /** 指定外部回调函数, 并使用该回调函数执行 TCTI / System API 上下文对象初始化 */
+    void setContextInitializer(ContextInitializer& initializer);
+    /**
+     * 与TPM2.0设备或本地Simulator模拟器守护进程建立连接
+     *
+     * 之前指定的上下文初始化
+     */
+    void connect();
+    /** 发送命令帧 */
+    void sendCommand(
+            TPMCommand& command ///< 输入参数. 此TPMCommand对象自带buildCmdPacket()组帧方法生成命令帧报文
+            );
+    /**
+     * 取回应答帧
+     *
+     * @note 该函数放在每条sendCommand()之后被调用. 若没有发送过命令帧, 则无法取回的应答桢.
+     * @note 若该函数执行成功, 返回的数据将被写入之前调用 sendCommand() 时指定的 command 对象.
+     *
+     * @throws TSS2_RC (可能遇到多种错误情况, 包括TSS层或TPM硬件返回的错误码) TODO: 此处需补充文档和样例代码帮助开发者处理不同的 TPM_RC/TSS2_RC 错误编码.
+     */
+    void fetchResponse(
+            int32_t timeout=-1 ///< 超时选项. 默认使用负数表示阻塞等待, 直到服务器端应答或者发生其他严重错误
+            );
+    /** 发送命令帧并取回应答帧 */
+    void sendCommandAndWaitUntilResponseIsFetched(
+            TPMCommand& cmd ///< 输入参数. 此TPMCommand对象自带buildCmdPacket()组帧方法生成命令帧报文
+            );
+    /**
+     * 切断与守护进程之间的通讯连接
+     *
+     * @details
+     * 优雅地切断之前建立的任意 socket 连接(通常是与 TSS resource manager 守护进程之间的链接)
+     * 底层调用 shutdown() 通知socket服务器端正常结束会话连接(connection), 然后调用 closesocket() 关闭本地套接字.
+     */
+    void disconnect();
+
+private:
+    TPMCommand *m_pLastCommand; ///< 内部成员变量. m_pLastCommand总是指向之前最后一次调用sendCommand()成员函数时的关联的TPMCommand参数的内存地址
+};
+
+/// 基于 TCP 套接字的 TCTI / System API 初始化工具
+class SocketBasedContextInitializer: public ContextInitializer {
+public:
+    /** 构造函数 */
+    SocketBasedContextInitializer(const char *hostname="127.0.0.1", uint16_t port=2321);
+    /** 析构函数 */
+    virtual ~SocketBasedContextInitializer();
+    /** 回调接口 */
+    virtual void initializerCallbackFunc(TSS2_TCTI_CONTEXT *, size_t, TSS2_SYS_CONTEXT *, size_t);
+private:
+    const char *m_hostname;
+    uint16_t m_port;
+};
+
+/// 直接读写 /dev/tpm0 设备的 TCTI / System API 初始化工具
+class DeviceBasedContextInitializer: public ContextInitializer {
+public:
+    /** 构造函数 */
+    DeviceBasedContextInitializer(const char *device="/dev/tpm0");
+    /** 析构函数 */
+    virtual ~DeviceBasedContextInitializer();
+    /** 回调接口 */
+    virtual void initializerCallbackFunc(TSS2_TCTI_CONTEXT *, size_t, TSS2_SYS_CONTEXT *, size_t);
+private:
+    const char *m_device;
+};
+
+#endif // __cplusplus
+#endif // CLIENT_H_
