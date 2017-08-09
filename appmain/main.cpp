773a052372421b5013cb48e71e141ed1f5189dae
liuqun <liuqun@cb060f17-8521-4c63-8ce0-ac1ab11b5b73>
2017-08-09 10:27:46 +0000

    appmain: 创建新的测试框架 HashTestDemoClient
    计划初步替换原有的TestCase旧框架
    新的框架将支持直接读写/dev/tpm0, 同时仍然支持Socket连接Simulator
    以及Socket连接历史遗留的resourcemgr守护进程. 暂不支持D-Bus连接tabrmd
    
    -- Liu Qun (liuqun) <qunliu@zyhx-group.com>
    
    git-svn-id: svn://192.168.0.210/srv/svn/repos/repo-1/trunk@228 cb060f17-8521-4c63-8ce0-ac1ab11b5b73

--- appmain/main.cpp
+++ appmain/main.cpp
@@ -15,6 +15,7 @@ using namespace std;
     const uint16_t DEFAULT_RESMGR_TPM_PORT=DEFAULT_SIMULATOR_TPM_PORT;
 #endif
 #include "TPMCommand.h"
+#include "Client.h"
 
 /* 排版格式: 以下函数均使用4个空格缩进，不使用Tab缩进 */
 
@@ -24,17 +25,40 @@ static void PrintHelp()
     printf("-rmhost 手动指定运行资源管理器(即 resourcemgr)的主机IP地址或主机名 (默认值: %s)\n",
             DEFAULT_HOSTNAME);
     printf("-rmport 手动指定运行资源管理器的主机端口号 (默认值: %d)\n", DEFAULT_RESMGR_TPM_PORT);
+    printf("-localTctiTest\n");
+    printf("[注意: 若使用 -localTctiTest 请手动关闭任何占用/dev/tpm0设备的进程, 即: 关闭其他直接访问/dev/tpm0的resourcemgr进程]\n");
 }
+
+#include <vector>
+using std::vector;
+#include <string>
+using std::string;
+#include <sstream>
+using std::stringstream;
+#include <stdexcept>
+using std::exception;
+using std::runtime_error;
 
 int main(int argc, char *argv[])
 {
     int count;
-    char *hostname = DEFAULT_HOSTNAME;
+    int usingDeviceFile = false;
+    const char *deviceFile = "/dev/tpm0";
+    const char *hostname = "127.0.0.1";
     uint16_t port = DEFAULT_RESMGR_TPM_PORT;
 
     count = 1;
     while (count < argc)
     {
+        if( 0 == strcmp(argv[count], "-localTctiTest" ) )
+        {
+            usingDeviceFile = true;
+            count += 1;
+            // 以上代码提供的命令行参数为: -localTctiTest
+            // 用于直接操作/dev/tpm0设备
+            continue;
+        }
+
         if (0 == strcmp(argv[count], "-rmhost"))
         {
             if (count + 1 >= argc)
@@ -63,6 +87,124 @@ int main(int argc, char *argv[])
         // 以上代码提供了一组简单的命令行参数便于调试:
         // 其中包括 [-rmhost IP地址] 和 [-rmport 端口号]
         // 如果不指定命令行参数, 则会直接连接到本机 IP 地址默认端口上运行的资源管理器
+    }
+
+    const char *szMsg = "abc";
+    const uint16_t nMsgLen = strlen(szMsg);
+    printf("测试输入字符串为szMsg='%s', 长度=%d字节\n", szMsg, (int)nMsgLen);
+
+    /// 计算单个数据包的哈希摘要, 输入数据的最大长度由TPM硬件以及TSS动态库限制, 通常为1024字节
+    class HashCalculatorClient: public Client
+    {
+    public:
+        /// 计算SHA256哈希摘要结果
+        ///
+        /// @return 哈希摘要结果, 格式为二进制数据, 类型为 const vector<BYTE>& C++ 指针引用
+        /// @throws std::exception 代表执行失败, 常见异常情况包括: 无法打开TPM设备文件/dev/tpm0或无法与Simulator建立Socket连接
+        const vector<BYTE>& SHA256(const void *data, ///< 指向输入数据的指针
+                UINT16 length ///< 数据长度. 单位: 字节. 取值范围[0, 1024], 单条输入数据的长度上限受TSS和物理硬件共同限制, 实际上限有可能小于1024字节
+                )
+        {
+            m_hashCmd.configHashAlgorithmUsingSHA256();
+            return sendHashCommandAndWaitUntilResponseIsFetched(data, length);
+        }
+
+    public:
+        /// 计算SHA1哈希摘要结果
+        ///
+        /// @return 哈希摘要结果, 格式为二进制数据, 类型为 const vector<BYTE>& C++ 指针引用
+        /// @throws std::exception 代表执行失败, 常见异常情况包括: 无法打开TPM设备文件/dev/tpm0或无法与Simulator建立Socket连接
+        const vector<BYTE>& SHA1(const void *data, ///< 指向输入数据的指针
+                UINT16 length ///< 数据长度. 单位: 字节. 取值范围[0, 1024], 单条输入数据的长度上限受TSS和物理硬件共同限制, 实际上限有可能小于1024字节
+                )
+        {
+            m_hashCmd.configHashAlgorithmUsingSHA1();
+            return sendHashCommandAndWaitUntilResponseIsFetched(data, length);
+        }
+
+    private:
+        /// 发送哈希命令然后取回摘要结果
+        ///
+        /// @return 哈希摘要结果, 格式为二进制数据, 类型为 const vector<BYTE>& C++ 指针引用
+        /// @throws std::exception 代表执行失败, 常见异常情况包括: 无法打开TPM设备文件/dev/tpm0或无法与Simulator建立Socket连接
+        const vector<BYTE>& sendHashCommandAndWaitUntilResponseIsFetched(
+                const void *data, ///< 指向输入数据的指针
+                UINT16 length ///< 数据长度. 单位: 字节. 取值范围[0, 1024], 单条输入数据的长度上限受TSS和物理硬件共同限制, 实际上限有可能小于1024字节
+                )
+        {
+            m_digest.clear();
+            try
+            {
+                m_hashCmd.configInputData(data, length);
+                sendCommandAndWaitUntilResponseIsFetched(m_hashCmd);
+                const TPM2B_DIGEST &outHash = m_hashCmd.outHash();
+                m_digest.assign(outHash.t.buffer, outHash.t.buffer+outHash.t.size);
+                return m_digest;
+            }
+            catch (...)
+            {
+                std::stringstream msg;
+                msg << "An unknown error was detected from " << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__;
+                throw std::runtime_error(msg.str());
+            }
+            return m_digest;
+        }
+
+    private:
+        /// 哈希摘要结果私有数据存储区
+        std::vector<BYTE> m_digest;
+
+    private:
+        /// 被测对象TPMCommands::Hash类
+        TPMCommands::Hash m_hashCmd;
+    };
+
+    HashCalculatorClient client;
+
+    SocketBasedContextInitializer socketTCTIContextInitializer(hostname, port);
+    DeviceBasedContextInitializer deviceTCTIContextInitializer(deviceFile);
+    if (usingDeviceFile)
+    {
+        client.setContextInitializer(deviceTCTIContextInitializer);
+    }
+    else
+    {
+        client.setContextInitializer(socketTCTIContextInitializer);
+    }
+
+    try
+    {
+        client.connect();
+
+        printf("输出SHA1哈希结果如下:\n");
+        {
+            const std::vector<BYTE>& digest =
+                    client.SHA1(szMsg, nMsgLen);
+            vector<BYTE>::const_iterator i;
+            for (i=digest.begin(); i!=digest.end(); i++)
+            {
+                printf("%02X:", (BYTE) *i);
+            }
+            printf("\n");
+        }
+        printf("输出SHA256哈希结果如下:\n");
+        {
+            const std::vector<BYTE>& digest =
+                    client.SHA256(szMsg, nMsgLen);
+            vector<BYTE>::const_iterator i;
+            for (i=digest.begin(); i!=digest.end(); i++)
+            {
+                printf("%02X:", (BYTE) *i);
+            }
+            printf("\n");
+        }
+
+        client.disconnect();
+    }
+    catch (std::exception& err)
+    {
+        fprintf(stderr, "Error: %s\n", err.what());
+        PrintHelp();
     }
 
     return (0);
