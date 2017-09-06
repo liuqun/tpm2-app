4bd3382b5d08e6512b98d9c017cd7e345e1154e0
liuqun <liuqun@cb060f17-8521-4c63-8ce0-ac1ab11b5b73>
2017-09-06 09:57:20 +0000

    appmain: 简单实现一个逐行读取文本文件的API函数
    GetNextLineFromTextFile()
    
    git-svn-id: svn://192.168.0.210/srv/svn/repos/repo-1/trunk@280 cb060f17-8521-4c63-8ce0-ac1ab11b5b73

---
+++ appmain/TextFileReader.cpp
@@ -0,0 +1,34 @@
+﻿/* encoding: utf-8 */
+// Copyright (c) 2017, 青岛中怡智能安全研究院有限公司
+// All rights reserved.
+
+#include <string>
+using std::string;
+#include <ios>
+using std::ios;
+#include "TextFileReader.h"
+
+// GetNextLineFromTextFile(fp, s, '\n') 从文件fp读取下一行内容存储到字符串s中, 指定'\n'作为换行标记符
+const string& GetNextLineFromTextFile(FILE *fpTextFileIn, string& sLineOut, char cLineDelimiter)
+{
+    int ch;
+    string line;
+
+    if (feof(fpTextFileIn))
+    {
+        throw ios::failure("Already at end of file! No more lines to read!");
+    }
+    while ((ch = fgetc(fpTextFileIn)) != EOF)
+    {
+        if (ch == cLineDelimiter)
+        {
+            break; // NOTE: 这里不额外存储行末的换行符
+        }
+        if (isprint(ch) || isblank(ch)) // FIXME: 任何非ASCII编码的字符都被当作无效字符直接忽略. (建议事先将汉字转换为Base64格式存储)
+        {
+            line += (char)ch;
+        }
+    }
+    sLineOut = line;
+    return(sLineOut);
+}
---
+++ appmain/TextFileReader.h
@@ -0,0 +1,24 @@
+﻿/* encoding: utf-8 */
+// Copyright (c) 2017, 青岛中怡智能安全研究院有限公司
+// All rights reserved.
+
+#ifndef TEXT_FILE_READER_H_
+#define TEXT_FILE_READER_H_
+
+#ifdef __cplusplus
+
+#include <cstdio>
+#include <string>
+
+/// 从文本文件读取下一行内容
+///
+/// 输出字符串的末尾不包括原始的换行符'\n'
+/// @param fpTextFileIn 文件指针
+/// @param cLineDelimiter 换行符
+/// @param sLineOut 输出字符串
+/// @return std::string 引用sLineOut
+/// @throws std::ios::failure 读取硬盘或SD等卡存储介质时IO操作失败. 参见C++头文件`#include<ios>`
+const std::string& GetNextLineFromTextFile(FILE *fpTextFileIn, std::string& sLineOut, char cLineDelimiter='\n');
+
+#endif // __cplusplus
+#endif // TEXT_FILE_READER_H_
